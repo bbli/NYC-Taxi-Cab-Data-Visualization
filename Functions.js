a6d44377e28ad4153f78a6276ff0c8013999eabc
Benson Li <bensonbinbinli@gmail.com>
2018-03-07 19:55:53 -0800

    Decided that scaling second map will cause complication in map() due to outliers

--- Functions.js
+++ Functions.js
@@ -1,3 +1,4 @@
+////////////////////////////////////////////////////////////////////////////
 function projectWorldCoordinates(datapoints,origin){
     // Projects longitude and latitude to
     // world mercator coordinates
@@ -11,21 +12,19 @@ function projectWorldCoordinates(datapoints,origin){
         point.dropoff = originTranslate(point.dropoff,projected_origin);
     }
 }
-////////////////////////////////////////////////////////////////////////////
 
 function project(array,zoom){
     let x = mercX(array[0],zoom);
     let y = mercY(array[1],zoom);
     return [x, y];
 }
-
+////////////////////
 function mercX(lon, zoom) {
 	lon = radians(lon);
 	var a = (256 / PI) * pow(2, zoom);
 	var b = lon + PI;
 	return a * b;
 }
-
 function mercY(lat,zoom) {
 	lat = radians(lat);
 	var a = (256 / PI) * pow(2, zoom);
@@ -33,13 +32,28 @@ function mercY(lat,zoom) {
 	var c = PI - log(b);
 	return a * c;
 }
-////////////////////////////////////////////////////////////////////////////
+////////////////////
 
 function originTranslate(array, projected_origin){
     x = array[0]-projected_origin[0];
     y = array[1]-projected_origin[1];
     return [x,y]
 }
+
+////////////////////////////////////////////////////////////////////////////
+//function scaleWorldCoordinates(datapoints){
+    //for (let point of datapoints){
+        //point.scaled_pickup = upperLeftCoordinates(point.pickup);
+        //point.scaled_pickup = scalemap(point.scaled_pickup);
+    //}
+//}
+
+
+//function upperLeftCoordinates(array){
+    //x=array[0]+g_plane_resolution[0]/2;
+    //y=array[1]+g_plane_resolution[1]/2;
+    //return [x,y]
+//}
 
 ////////////////////////////////////////////////////////////////////////////
 
