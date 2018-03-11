5b864f03c3ebeeadb5fe70155b0e47538ef34367
Benson Li <bensonbinbinli@gmail.com>
2018-03-10 22:41:47 -0800

    added global cutoff variables and updated dataset for these cutoffs

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 //User defined
 let system;
+let gui;
 
 //Setup
 let mapimg;
@@ -12,34 +13,41 @@ let g_zoom = 10.6;
 let g_origin=[-73.92756,40.75318];
 //g_plane_resolution controls the size of the image we get from mapbox
 //and the plane we create
-let g_plane_resolution= [512,512];
-let g_max_payment = 158;
+let g_plane_resolution= [1024,1024];
+let g_payment_cutoff = 75;
+let g_time_cutoff = 90;//Still need to use this to map curve tightness
+let g_z_offset = 200;
 
 
 function preload(){
     table = loadTable('trial_data.csv','csv','header');
+    colorbar = loadImage('colormapManip_14.png')
 }
 
 function setup() {
 	createCanvas(windowWidth, windowHeight,WEBGL);
 
-    colorbar = loadImage('./data/colormapManip_14.png')
-
-    //mapimg = loadImage('https://api.mapbox.com/styles/v1/mapbox/light-v9/static/'
-        //+g_origin[0]+','+g_origin[1]+','+g_zoom+',0,0/'+g_plane_resolution[0]+'x'+g_plane_resolution[1]+
-        //'?access_token=pk.eyJ1IjoiYmJsaSIsImEiOiJjamVjYXJsMGUwaHluMzNvZDE4MmkxYW40In0.AWDPnfoCzeVRY78xDLyjTQ');
+    mapimg = loadImage('https://api.mapbox.com/styles/v1/mapbox/light-v9/static/'
+        +g_origin[0]+','+g_origin[1]+','+g_zoom+',0,0/'+g_plane_resolution[0]+'x'+g_plane_resolution[1]+
+        '?access_token=pk.eyJ1IjoiYmJsaSIsImEiOiJjamVjYXJsMGUwaHluMzNvZDE4MmkxYW40In0.AWDPnfoCzeVRY78xDLyjTQ');
     
-	easycam = createEasyCam(p5.RendererGL,{distance:400});
+    easycam = createEasyCam(p5.RendererGL,{distance:2500});
+    //easycam = createEasyCam();
     system = new DatapointSystem(table);
-    
 
 }
 
 function draw() {
 	background(200);
 
+    system.plot();
+
+    texture(mapimg);
+    plane(g_plane_resolution[0],g_plane_resolution[1]);
+
+
     push();
-    //texture(mapimg);
+    translate(0,0,g_z_offset);
     plane(g_plane_resolution[0],g_plane_resolution[1]);
     pop();
 
