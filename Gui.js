938d4567a27908a29e276eb9682759e933bc181c
Benson Li <bensonbinbinli@gmail.com>
2018-03-20 20:20:22 -0700

    Refactored buttons into their own class

--- Gui.js
+++ Gui.js
@@ -1,4 +1,4 @@
-function Slider(max,index,string,start_height){
+function Slider(max,index,string,start_width,start_height){
     let slider_width = 200
     let slider_seperation = 60;
     let slider_height = 50;
@@ -9,8 +9,8 @@ function Slider(max,index,string,start_height){
     //for every change
     this.value = createP(string+": "+this.slider.value());
 
-    this.slider.position(50,start_height+offset+slider_height+(index+1)*slider_seperation)
-    this.value.position(slider_width+20+50,start_height+offset+slider_height+(index+1)*slider_seperation);
+    this.slider.position(start_width,start_height+offset+slider_height+(index+1)*slider_seperation)
+    this.value.position(slider_width+20+start_width,start_height+offset+slider_height+(index+1)*slider_seperation);
     
     this.slider.size(slider_width,slider_height);
     this.slider.addClass('slider');
@@ -24,56 +24,30 @@ function Slider(max,index,string,start_height){
     this.slider.input(this.updateValue);
 }
 
+function Button(index,string,start_width, start_height,callback){
+    let padding =20;
+    let button_height = 110;
+    let button_width = 200;
+
+    this.button = createButton(string);
+    this.button.position(start_width,start_height+index*button_height+index*padding);
+    this.button.size(button_width,button_height);
+    this.button.mousePressed(callback);
+}
 
 function MainGui(system){
+    //this.title = createElement('h1', 'Curved Time Paths')
+    //So Gui can talk with system
+    this.system = system;
+    //Used to update slider values display
+    this.slider_list = [];
     //Used to turn display on/off
     this.html_elements_list =[];
-    
-    this.max_list = [g_payment_cutoff,g_payment_cutoff,g_time_cutoff,g_time_cutoff];
-    this.slider_names_list = ["Min Payment", "Max Payment", "Min Time", "Max Time"];
-    this.slider_list = [];
   ////////////////////////////////////////////////////////////////////////////
-    let start_height = 60;
-    for (let i=0; i<4; i++){
-        var slider = new Slider(this.max_list[i],i,this.slider_names_list[i],start_height);
-        this.slider_list.push(slider);
-        this.html_elements_list.push(slider);
-    }
-
-    this.system = system;
-    //this.title = createElement('h1', 'Curved Time Paths')
-    this.filter_button = createButton("Time/Payment");
-    this.html_elements_list.push(this.filter_button);
-    this.top_map_button = createButton("Display Top Map");
-    this.html_elements_list.push(this.top_map_button);
-    this.pickup_button = createButton("Pickup");
-    this.html_elements_list.push(this.pickup_button);
-    this.dropoff_button = createButton("Dropoff");
-    this.html_elements_list.push(this.dropoff_button);
-    
-
     this.pg = createGraphics(400,100);
     this.pg.background(255,0);
     this.pg.textSize(40);
     this.pg.text("Curved Taxi Paths",30,50);
-
-    /////////////////////POSITIONS////////////////////////
-    //this.title.position(windowWidth/2,50);
-    let button_width = 200
-    let button_height = 110;
-    let padding =20;
-    this.top_map_button.position(windowWidth-button_width,start_height);
-    this.filter_button.position(50,start_height);
-    this.pickup_button.position(windowWidth-button_width,start_height+button_height+padding);
-    this.dropoff_button.position(windowWidth-button_width,start_height+2*button_height+2*padding);
-
-    /////////////////////SIZES////////////////////////
-    this.filter_button.size(button_width,button_height);
-    this.top_map_button.size(button_width,button_height);
-    this.pickup_button.size(button_width,button_height);
-    this.dropoff_button.size(button_width,button_height);
-
-
     /////////////////////METHODS////////////////////////
     this.toggleTopMap = () => {
         g_toggle_top_map = (!g_toggle_top_map);
@@ -89,10 +63,28 @@ function MainGui(system){
             system.filterDisplay(...value_list);
         }
     }
-
     /////////////////////Callbacks////////////////////////
-    this.top_map_button.mousePressed(this.toggleTopMap);
-    this.filter_button.mousePressed(this.updateDisplay);
+    /////////////////////INITALIZING SLIDERS////////////////////////
+    let start_height = 60;
+    let start_width = 60;
+    let max_list = [g_payment_cutoff,g_payment_cutoff,g_time_cutoff,g_time_cutoff];
+    let slider_names_list = ["Min Payment", "Max Payment", "Min Time", "Max Time"];
+    for (let i=0; i<4; i++){
+        let slider = new Slider(max_list[i],i,slider_names_list[i],start_width,start_height);
+        this.slider_list.push(slider);
+        this.html_elements_list.push(slider);
+    }
+    /////////////////////INITALIZING BUTTONS////////////////////////
+    //Except Time/Payment button
+    this.html_elements_list.push(new Button(0,"Time/Payment",start_width,start_height,this.updateDisplay));
+
+    let callback_list = [this.toggleTopMap, this.toggleTopMap, this.toggleTopMap];
+    let button_names_list=["Display Top Map", "Pickup", "Dropoff"];
+    for (let i=0; i<3; i++){
+        //NOT FINISHED
+        let button = new Button(i,button_names_list[i],windowWidth-210,start_height,callback_list[i]);
+        this.html_elements_list.push(button);
+    }
 
 }
 MainGui.prototype.displayTitle = function() {
